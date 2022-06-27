c253ae2cfbc6964a7112e6222fc248a9b57b767e
Erick Luciano <erickhbd@gmail.com>
2022-06-27 00:15:18 -0300

    mudança no tamanho e tentativa de colocar score e bg infinito

--- js/script.js
+++ js/script.js
@@ -1,12 +1,54 @@
 const mario = document.querySelector(".super-flash");
 const pipe = document.querySelector(".flash-reverso");
+const game = document.querySelector("game");
+
+//tentativaplacar 1
+/*function criaPlacar(){
+  const placar = {
+    pontuacao: 0,
+    desenha(){
+      contexto.font = '30px "VT323"';
+      contexto.fillStyle = 'black';
+      contexto.fillStyle('placar: ${placar.pontuacao}');
+    },
+    atualiza(){
+    }
+  }
+  return placar;
+}*/
+
+//tentativa de placar #2
+/*let scoreValue = 0;
+let counter = true;
+const loopGame = function (){
+  const loopGame = setInterval(()=> {
+    const pipePosition = pipeOffsetLeft;
+    const characterPosition = +window
+    .getComputedStyle(character)
+    .bottom.replace("px", "")
+
+    if (pipePosition = pipe.offsetLeft <=-120 && counter ) {
+      scoreValue += 10;
+      score.textContent = scoreValue;
+      counter = false;
+    }
+    else if ( pipePosition - characterPosition >0) {
+      counter = true;
+    }
+    else {
+
+    }
+    
+  })
+  
+}*/
 
 const jump = () => {
   mario.classList.add("jump-mario");
 
   setTimeout(() => {
     mario.classList.remove("jump-mario");
-  }, 500);
+  }, 600);
 };
 
 const loopGame = setInterval(() => {
@@ -26,9 +68,7 @@ const loopGame = setInterval(() => {
     mario.style.width = "180px";
     mario.style.marginLeft = "-100/*px";
     
-    
-
-    clearInterval(loopGame);
+     clearInterval(loopGame);
   }
 }, 10);
 
