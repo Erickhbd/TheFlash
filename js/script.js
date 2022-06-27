ca83afe45c34e3187e3ef7b36b8886011ca1e2b1
Erick Luciano <erickhbd@gmail.com>
2022-06-27 17:08:56 -0300

    correção de falhas

--- js/script.js
+++ js/script.js
@@ -1,47 +1,6 @@
 const mario = document.querySelector(".super-flash");
 const pipe = document.querySelector(".flash-reverso");
-const game = document.querySelector("game");
 
-//tentativaplacar 1
-/*function criaPlacar(){
-  const placar = {
-    pontuacao: 0,
-    desenha(){
-      contexto.font = '30px "VT323"';
-      contexto.fillStyle = 'black';
-      contexto.fillStyle('placar: ${placar.pontuacao}');
-    },
-    atualiza(){
-    }
-  }
-  return placar;
-}*/
-
-//tentativa de placar #2
-/*let scoreValue = 0;
-let counter = true;
-const loopGame = function (){
-  const loopGame = setInterval(()=> {
-    const pipePosition = pipeOffsetLeft;
-    const characterPosition = +window
-    .getComputedStyle(character)
-    .bottom.replace("px", "")
-
-    if (pipePosition = pipe.offsetLeft <=-120 && counter ) {
-      scoreValue += 10;
-      score.textContent = scoreValue;
-      counter = false;
-    }
-    else if ( pipePosition - characterPosition >0) {
-      counter = true;
-    }
-    else {
-
-    }
-    
-  })
-  
-}*/
 
 const jump = () => {
   mario.classList.add("jump-mario");
@@ -57,16 +16,16 @@ const loopGame = setInterval(() => {
     .getComputedStyle(mario)
     .bottom.replace("px", "");
 
-  if (pipePosition <= 180 && pipePosition > 0 && marioPosition < 80) {
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
 
     mario.style.animation = "none";
     mario.style.bottom = `${marioPosition}px`;
 
-    mario.src = "./images/flash-game-over.png";
-    mario.style.width = "180px";
-    mario.style.marginLeft = "-100/*px";
+    mario.src = "./imagens/flash-game-over.png";
+    mario.style.width = "140px";
+    mario.style.marginRight = "0/*px";
     
      clearInterval(loopGame);
   }
